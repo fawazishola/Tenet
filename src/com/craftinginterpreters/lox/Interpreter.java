152c036909559bf7ad5a8f61f7c2b7c46b7d3afd
FawazIshola <60826942+fawazishola@users.noreply.github.com>
2025-12-08 23:46:43 -0500

    Update Interpreter.java

---
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -0,0 +1,35 @@
+package com.craftinginterpreters.lox;
+
+class Interpreter implements Expr.Visitor<Object> {
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        return expr.value;
+
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return evaluate(expr.expression);
+    }
+
+    private Object evaluate(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public Object vistiUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return -(double) right;
+            case BANG:
+                return !isTruthy(right);
+        }
+
+        return null;
+
+    }
+
+}
